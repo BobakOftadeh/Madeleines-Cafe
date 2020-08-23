7f1e2ea4e05b7f54ed2977f3a18d7847232c60a5
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-08-23 12:45:17 -0400

    feat:initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
