2a39fa5ba336ceadbf779da72fbb90ffb69481fb
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-08-25 20:16:04 -0400

    feat: implemented footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,11 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
   background-color: var(--color-primary-dark);
+  color: white;
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
 `;
 
 const Footer = () => {
-  return <FooterWrapper></FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <h1>dine</h1>
+      <div>
+        <h3>MARTHWAITE, SEDBERGH</h3>
+        <h3>CUMBRIA</h3>
+        <h3>+00 44 416 7878</h3>
+      </div>
+      <div>
+        <h3>OPEN TIMES</h3>
+        <h3>MON - FRI: 09:00 AM - 10:00 PM</h3>
+        <h3>SAT - SUN: 09:00 AM - 11:30 PM</h3>
+      </div>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
--- src/components/Reservation/index.tsx
+++ src/components/Reservation/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '../Button';
+import ReservationBg from '../../images/cafe2.jpg';
 
 const ReservationsWrapper = styled.section`
-  background-color: rgba(57, 62, 70, 0.5);
-  background-image: url(../../images/cafe2.jpg);
+  background-image: linear-gradient(
+      rgba(57, 62, 70, 0.7),
+      rgba(57, 62, 70, 0.7)
+    ),
+    url(${ReservationBg});
+  background-position: center;
+  background-attachment: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h3 {
+    color: white;
+    font-size: 4rem;
+    margin-left: -8%;
+    margin-right: 21%;
+  }
 `;
 
 const Reservations: React.FC = () => {
