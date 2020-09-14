fda6b3202fe227bdfaa5f2d6ffd442499ae99bb8
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-09-14 01:12:20 -0400

    Updates

---
+++ precache-manifest.41caf001342692ec7e8efe3261b8013f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b328bafc9344e36b0ee7e3b9a43b6a46",
+    "url": "/Madeleines-Cafe/index.html"
+  },
+  {
+    "revision": "afb894ac043a51d83fad",
+    "url": "/Madeleines-Cafe/static/js/2.e3ae5780.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Madeleines-Cafe/static/js/2.e3ae5780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8b6907045775f75f74",
+    "url": "/Madeleines-Cafe/static/js/main.51dab2c6.chunk.js"
+  },
+  {
+    "revision": "a2406072b457464024bd",
+    "url": "/Madeleines-Cafe/static/js/runtime-main.ef58f6c1.js"
+  },
+  {
+    "revision": "aab3cc8ac8d7c7bb7fa7a91a8aa0ecda",
+    "url": "/Madeleines-Cafe/static/media/cafe1.aab3cc8a.jpg"
+  },
+  {
+    "revision": "370914ef75ddf9a9eab7c8d7f7e3552d",
+    "url": "/Madeleines-Cafe/static/media/cafe2.370914ef.jpg"
+  },
+  {
+    "revision": "21f852843627f5b7644cd2d09dd63412",
+    "url": "/Madeleines-Cafe/static/media/cafe3.21f85284.jpg"
+  },
+  {
+    "revision": "6d7ab9fd6c95c4261d6c1c0153551f80",
+    "url": "/Madeleines-Cafe/static/media/cafeVideo1.6d7ab9fd.mp4"
+  },
+  {
+    "revision": "7887e3e70eff5bfdd61bbb2f00bbbe28",
+    "url": "/Madeleines-Cafe/static/media/cafeVideo2.7887e3e7.mp4"
+  },
+  {
+    "revision": "8b8a7b4ba2fa51dc886fd9efaefd01cb",
+    "url": "/Madeleines-Cafe/static/media/coffee.8b8a7b4b.jpg"
+  },
+  {
+    "revision": "a54490a5d79736d4eefd8e0283afd859",
+    "url": "/Madeleines-Cafe/static/media/food1.a54490a5.jpg"
+  },
+  {
+    "revision": "44c5ed157cd751d53b2b3586c0c90537",
+    "url": "/Madeleines-Cafe/static/media/food2.44c5ed15.jpg"
+  }
+]);
